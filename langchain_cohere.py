1b8e83332f3c73d883e758a50482087fd1115d1b
Srushti Kadalagi <150148006+srushti-kadalagi@users.noreply.github.com>
2025-03-30 23:29:24 +0530

    Update langchain_cohere.py
    added comments

--- langchain_cohere.py
+++ langchain_cohere.py
@@ -1,10 +1,10 @@
 from langchain_community.llms import Cohere
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
-from load_drive_doc import text_content  # Import the text from Google Drive
+from load_drive_doc import text_content  #  To import the text from Google Drive
 
-# Set up Cohere API
-cohere_api_key = "t3h2NuHyMNLBBEtjKzsMQDPA5JEokWoMTwKGAvPP"
+
+cohere_api_key = "t3h2NuHyMNLBBEtjKzsMQDPA5JEokWoMTwKGAvPP" # setting up cohere API
 llm = Cohere(cohere_api_key=cohere_api_key, model="command")
 
 # Create a prompt template
@@ -13,9 +13,9 @@ prompt_template = PromptTemplate(
     template="Summarize the following text:\n\n{document}\n\nSummary:"
 )
 
-# Create a chain to process text
+
 chain = LLMChain(llm=llm, prompt=prompt_template)
 
-# Run the model
+
 output = chain.run(document=text_content)
 print("\nGenerated Summary:\n", output)
